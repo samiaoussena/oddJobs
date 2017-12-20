2af6cef2a1f09a4398b3ef4d104e6dfa0b81d664
samiaoussena <samia.oussena@uwl.ac.uk>
2017-12-20 14:56:20 +0000

    fixed main

--- main.go
+++ main.go
@@ -1,19 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
-	service "github.com/samiaoussena/CI_OddJobs/service"
+	"github.com/cloudfoundry-community/go-cfenv"
+	"github.com/samiaoussena/CI_OddJobs/service"
 )
 
 func main() {
 	port := os.Getenv("PORT")
 	if len(port) == 0 {
-		port = "3001"
+		port = "3000"
 	}
 
-	// Ordinarily we'd use a CF environment here, but we don't need it for
-	// the fake data we're returning.
-	server := service.NewServer()
+	appEnv, err := cfenv.Current()
+	if err != nil {
+		fmt.Println("CF Environment not detected.")
+	}
+
+	server := service.NewServerFromCFEnv(appEnv)
 	server.Run(":" + port)
 }
